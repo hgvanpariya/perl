8be5a56e522402d1488411cf7b34283a7469946c
Harikrushna <h.g.vanpariya@gmail.com>
2014-03-29 11:42:16 -0700

    Write file using Perl

--- WriteFile.pl
+++ WriteFile.pl
@@ -1,4 +1,8 @@
 #!/usr/bin/perl
+## Write File using Perl
+## Here outputfile.txt is the file which will be generate as output file.
 open (MYOUTFILE, '>>outputfile.txt');
+## MYOUTFILE is the object which will mapped with the output file name.
 print MYOUTFILE "Hello World !!\n";
+## Close the the file before exit.
 close (MYOUTFILE); 
